61cca7f1ec9068a61b8a9c8a20980f3af953bb63
Hicham Bendarbi <49118568+hichambendarbi@users.noreply.github.com>
2020-05-18 16:56:54 +0100

    initialise e-commerce

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
-import Routes from './components/routing/Routes'
+
 import './App.css';
 
-
-// Redux
-import { Provider } from 'react-redux'
-import store from './store'
-
-
-if(localStorage.token) {
-  setAuthToken(localStorage.token)
-}
 const App = () =>{
-  useEffect(()=> {
-    store.dispatch(loadUser());
-  }, []);
   return (
   <Provider store={store}>
-  <Router>
-      <Fragment>
-          <Navbar />
-          <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route component={Routes}/>
-          </Switch>
-      </Fragment>
-  </Router>
+           <div>TEST</div>
   </Provider>
   )
 }
